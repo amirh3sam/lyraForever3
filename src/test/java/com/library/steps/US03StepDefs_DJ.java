1e2505e7236a72fc08b507d5d449b81370a77eec
desi0610 <desita_mail@yahoo.com>
2023-03-27 21:06:11 -0400

    my US3 update

--- src/test/java/com/library/steps/US03StepDefs_DJ.java
+++ src/test/java/com/library/steps/US03StepDefs_DJ.java
@@ -3,12 +3,18 @@ package com.library.steps;
 import com.library.pages.BookPage;
 import com.library.pages.LoginPage;
 import com.library.utility.BrowserUtil;
+import com.library.utility.DB_Util;
 import io.cucumber.java.en.*;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class US03StepDefs_DJ {
 
     LoginPage loginPage = new LoginPage();
     BookPage bookPage = new BookPage();
+    List<String> actualList;
     @Given("the {string} on the home page")
     public void the_on_the_home_page(String librarian) {
         loginPage.login(librarian);
@@ -29,6 +35,16 @@ public class US03StepDefs_DJ {
 
     @Then("verify book categories must match book_categories table from db")
     public void verify_book_categories_must_match_book_categories_table_from_db() {
+
+        Select select = new Select(bookPage.mainCategoryElement);
+        actualList = BrowserUtil.getElementsText(select.getOptions());
+        actualList.remove(0);
+
+        String query = "select name from book_categories";
+        DB_Util.runQuery(query);
+        List<String> expectedList = DB_Util.getColumnDataAsList("name");
+        //expectedList.remove(0);
+        Assert.assertEquals(expectedList,actualList);
 
 
     }
