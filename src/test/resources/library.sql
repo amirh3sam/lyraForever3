0003a06d0c63fc881c5c30866c386f50c6a8399c
AmirHesamB <amirh3sam.badiei@gmail.com>
2023-03-28 20:27:38 -0400

    US 5 commit

--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -18,9 +18,15 @@ from users;
 
 
 -- US 03
-select count(*)
-from book_borrow
-where is_returned = 0;
+select * from book_borrow
+join book_categories ;
+
+
+select bc.name,count(*) from book_borrow bb
+                                 inner  join books b on bb.book_id = b.id
+                                 inner join book_categories bc on b.book_category_id=bc.id
+group by name
+order by 2 desc;
 
 
 -- US06
@@ -41,3 +47,4 @@ where full_name='Test Student 5'
 order by 3 desc;
 
 
+select
